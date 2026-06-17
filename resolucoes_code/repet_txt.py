75c5b91022936248967d86618277ff6f86fa7dbb
Danilo Palacio <114814797+Danilo-Palacio@users.noreply.github.com>
2026-06-16 23:22:06 -0300

    Add text repetition functionality in repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,5 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+texto = input("Digite um texto: ")
+quantidade = int(input("Digite a quantidade de repetições: "))
+
+print(texto * quantidade)
